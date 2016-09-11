8bfc3647c59b6e6c13ed9ebd6c41f887e693e0af
jinxingwang <wjx101220@hotmail.com>
2016-09-10 21:09:48 -0700

    add two kind cell button on cell

---
+++ Bc2Ride/CustomCell2.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell2.swift
+//  Bc2Ride
+//
+//  Created by jinxingwang on 9/9/16.
+//  Copyright © 2016 jinxingwang. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
